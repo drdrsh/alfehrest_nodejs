e88f63e8c164355ece10537e207207ae9850d9c8
Mostafa Abdelraouf <mostafa@mostafa.io>
2016-03-30 15:17:19 -0400

    added -f flag to allow running more than one app from the same codebase

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ pm2 stop $1
 pm2 delete $1
 sudo node database-server.js stop $1
 sudo node database-server.js start $1
-pm2 start app.js --name $1 -- --NODE_ENV=$1 --appId=$1
+pm2 start app.js -f --name $1 -- --NODE_ENV=$2 --appId=$1
 
